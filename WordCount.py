700abd8a11a401201249257eee3604ee437cb51c
TWoosley730 <twoosley@unomaha.edu>
2025-03-02 21:28:43 -0600

    Close

--- WordCount.py
+++ WordCount.py
@@ -1,14 +1,23 @@
 #WordCount.py
-#Name:
-#Date:
-#Assignment:
+#Name: Trevor Woosley
+#Date: 03/02/2025
+#Assignment: Wordcount
 
 def main():
   textFile = open("gettysberg.txt", 'r')
+  lineCount = 0
+  wordCount = 0
+  letterCount = 0
+
   
   for line in textFile:
-    print(line)
-  
+    lineCount = lineCount + 1
+    words = line.split()
+    for w in words:
+      wordCount = wordCount + 1
+    words = line.split
+  print("Lines:", lineCount)
+  print("Words:", wordCount)
 
 if __name__ == '__main__':
   main()
